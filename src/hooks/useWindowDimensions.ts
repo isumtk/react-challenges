4141d0af8cf74c482fb02feeca533aa47e372bfa
Sumit Kumar <krrsumit@outlook.com>
2023-03-19 09:36:10 +0530

    fixed hook

--- src/hooks/useWindowDimensions.ts
+++ src/hooks/useWindowDimensions.ts
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 
 const useWindowDimensions = () => {
-  const [width, setWidth] = useState<number>(0);
-  const [height, setHeight] = useState<number>(0);
+  const [width, setWidth] = useState<number>(window.innerWidth);
+  const [height, setHeight] = useState<number>(window.innerHeight);
 
   useEffect(() => {
     const trackDimensions = () => {
